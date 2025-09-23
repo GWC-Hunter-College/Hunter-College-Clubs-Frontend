2b9bfc0b63b84d19a62f7bb69985f538c77deebb
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2025-09-23 16:14:05 -0400

    placeholder elements for club directory used ai to make a placeholder of the elements in the club dir page routed buttons to club and event creation page skeletons for creation pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Home from "./pages/Home";
 import ClubDirectory from './pages/ClubDirectory';
 import Club from "./pages/Club";
 import Event from "./pages/Event";
+import ClubCreate from "./pages/ClubCreate"
+import EventCreate from "./pages/EventCreate"
 function App() {
 
 
@@ -14,6 +16,8 @@ function App() {
       <Route path="/clubs" element={<ClubDirectory />} />
       <Route path="/club/:clubId" element={<Club />} />
       <Route path="/event/:eventId" element={<Event />} />
+      <Route path="/club/create" element={<ClubCreate />} />
+      <Route path="/event/create" element={<EventCreate />} />
       <Route path="/auth" element={<Auth />} />
     </Routes>
     
